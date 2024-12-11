b0e3faca83d5e5dc6d76e423d6fa08bb4ecfa6d7
3alTemp <trialtemp.mailer@gmail.com>
2024-12-11 00:06:08 -0600

    dumbass messes replace code, asked to leave square hair

--- ba_data/python/bascenev1lib/__init__.py
+++ ba_data/python/bascenev1lib/__init__.py
@@ -3,3 +3,6 @@
 """Library of stuff using the bascenev1 api: games, actors, etc."""
 
 # ba_meta require api 8
+
+# Load the main Explodinary module here.
+import bse
--- ba_data/python/bse/.basereplace/replacement/bascenev1lib/__init__.py
+++
@@ -1,8 +0,0 @@
-# Released under the MIT License. See LICENSE for details.
-#
-"""Library of stuff using the bascenev1 api: games, actors, etc."""
-
-# ba_meta require api 8
-
-# Load the main Explodinary module here.
-import bse
